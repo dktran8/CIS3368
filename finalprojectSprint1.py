5f5928d67595c0fbf2aa6ebaa1d647c04448e4ce
dktran8 <77808486+dktran8@users.noreply.github.com>
2021-10-15 10:55:33 -0500

    Add files via upload

---
+++ finalprojectSprint1.py
@@ -0,0 +1,138 @@
+"""
+Dinh Tran
+CIS 3368
+Professor Otto Dobretsberger
+Final Project Sprint 1
+"""
+
+import mysql.connector
+import flask
+from flask import jsonify
+from flask import request, make_response
+from mysql.connector import Error
+from sql1 import create_connection
+from sql1 import execute_query
+from sql1 import execute_read_query
+
+#setting up an application name
+app = flask.Flask(__name__) #sets up the application
+app.config["DEBUG"] = True #allow to show errors in browser
+
+#Prints the entire guest table
+@app.route('/guests', methods=['GET'])
+def people():
+    conn = create_connection("cis3368.cdqrwblrgkaj.us-east-2.rds.amazonaws.com", "schoolproject", "cis3368fall", "cis3368fall21")
+    #Selects all data from the table
+    sql = "SELECT * FROM guest"
+    guest = execute_read_query(conn,sql)
+    #Results all data to return
+    results = []
+    for people in guest:
+        results.append(people)
+    return jsonify(results)
+
+#Add a new guest to the table
+@app.route('/api/addguest', methods=['POST'])
+#Request data to be added
+def addguest():
+    request_data = request.get_json()
+    newfirstname = request_data['firstname']
+    newlastname = request_data['lastname']
+    
+    #connects to mySQL database and allows to add data from Postman to mySQL
+    conn = create_connection("cis3368.cdqrwblrgkaj.us-east-2.rds.amazonaws.com", "schoolproject", "cis3368fall", "cis3368fall21")
+    query = "INSERT INTO guest (firstname, lastname) VALUES ('%s', '%s')" % (newfirstname, newlastname)
+    execute_query(conn,query)
+    return 'POST REQUEST WORKED'
+
+
+#Delete a guest in the table
+@app.route('/api/deleteguest', methods=['DELETE'])
+def deleteguest():
+    request_data = request.get_json()
+    deletename = request_data['firstname']
+    #connects to mySQL database and allows to modify data from Postman to mySQL
+    conn = create_connection("cis3368.cdqrwblrgkaj.us-east-2.rds.amazonaws.com", "schoolproject", "cis3368fall", "cis3368fall21")
+    query = "DELETE from guest WHERE firstname = '%s' " % (deletename)
+    execute_query(conn,query)
+    return 'Guest was successfully deleted.' #Returns a statement if the code works successfully
+
+#Update a guest in the table
+@app.route('/api/updateguest', methods=['PUT'])
+def updateguest():
+    request_data = request.get_json()
+    guestid = request_data['id']
+    updatefirstname = request_data['firstname']
+    updatelastname = request_data['lastname']
+    #connects to mySQL database and allows to modify data from Postman to mySQL
+    conn = create_connection("cis3368.cdqrwblrgkaj.us-east-2.rds.amazonaws.com", "schoolproject", "cis3368fall", "cis3368fall21")
+    query = "UPDATE guest SET firstname = '%s', lastname = '%s' WHERE id = %s" %(updatefirstname, updatelastname,guestid)
+    execute_query(conn,query)
+    return 'Guest was successfully updated'
+
+
+#Print the entire restaurant table:
+@app.route('/restaurants', methods=['GET'])
+def place():
+    conn = create_connection("cis3368.cdqrwblrgkaj.us-east-2.rds.amazonaws.com", "schoolproject", "cis3368fall", "cis3368fall21")
+    #Selects all data from the table
+    sql = "SELECT * FROM restaurant"
+    restaurant = execute_read_query(conn,sql)
+    #Results all data to return
+    results = []
+    for place in restaurant:
+        results.append(place)
+    return jsonify(results)
+
+#Add a new restaurant to the table
+@app.route('/api/addrestaurant', methods=['POST'])
+#Request data to be added
+def addrestaurant():
+    request_data = request.get_json()
+    newname = request_data['restaurantname']
+    
+    #connects to mySQL database and allows to add data from Postman to mySQL
+    conn = create_connection("cis3368.cdqrwblrgkaj.us-east-2.rds.amazonaws.com", "schoolproject", "cis3368fall", "cis3368fall21")
+    query = "INSERT INTO restaurant (restaurantname) VALUES ('%s')" % (newname)
+    execute_query(conn,query)
+    return 'POST REQUEST WORKED'
+
+#Delete a restaurant in the table
+@app.route('/api/deleterestaurant', methods=['DELETE'])
+def deleterestaurant():
+    request_data = request.get_json()
+    deletename = request_data['restaurantname']
+    #connects to mySQL database and allows to modify data from Postman to mySQL
+    conn = create_connection("cis3368.cdqrwblrgkaj.us-east-2.rds.amazonaws.com", "schoolproject", "cis3368fall", "cis3368fall21")
+    query = "DELETE from restaurant WHERE restaurantname = '%s' " % (deletename)
+    execute_query(conn,query)
+    return 'Restaurant was successfully deleted.' #Returns a statement if the code works successfully
+
+#Update a restaurant in the table
+@app.route('/api/updaterestaurant', methods=['PUT'])
+def updaterestaurant():
+    request_data = request.get_json()
+    restaurantid = request_data['id']
+    updatename = request_data['restaurantname']
+   
+    #connects to mySQL database and allows to modify data from Postman to mySQL
+    conn = create_connection("cis3368.cdqrwblrgkaj.us-east-2.rds.amazonaws.com", "schoolproject", "cis3368fall", "cis3368fall21")
+    query = "UPDATE restaurant SET restaurantname= '%s' WHERE id = %s" %(updatename, restaurantid)
+    execute_query(conn,query)
+    return 'Restaurant was successfully updated'
+
+
+#Generates a random restaurant
+@app.route('/api/randomrestaurant', methods=["GET"])
+def randomrestaurant():
+    conn = create_connection("cis3368.cdqrwblrgkaj.us-east-2.rds.amazonaws.com", "schoolproject", "cis3368fall", "cis3368fall21")
+    sql = "SELECT restaurantname FROM restaurant ORDER BY RAND () LIMIT 1"
+    random_rest = execute_read_query(conn, sql)
+    results = []
+    for object in random_rest:
+        results.append(object)
+    return jsonify(results)
+    
+
+
+app.run()
