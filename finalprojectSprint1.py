8eb5c49c332e2f45e608d964a1d986d9a1cf1183
dktran8 <77808486+dktran8@users.noreply.github.com>
2021-10-16 09:27:41 -0500

    Add files via upload

--- finalprojectSprint1.py
+++ finalprojectSprint1.py
@@ -38,11 +38,29 @@ def addguest():
     request_data = request.get_json()
     newfirstname = request_data['firstname']
     newlastname = request_data['lastname']
+    newrestaurant = request_data['restaurantname']
     
     #connects to mySQL database and allows to add data from Postman to mySQL
     conn = create_connection("cis3368.cdqrwblrgkaj.us-east-2.rds.amazonaws.com", "schoolproject", "cis3368fall", "cis3368fall21")
     query = "INSERT INTO guest (firstname, lastname) VALUES ('%s', '%s')" % (newfirstname, newlastname)
     execute_query(conn,query)
+
+    #make a while loop to ask user to input from 5 to 10 restaurants
+    count = 0
+    while (count <= 10):
+        print("Type a list of restaurants with 5 being minimum and 10 beng maximum (Type 'Q' to quit): ")
+        newrestaurant = input(" ")
+        count += 1
+        if newrestaurant == 'Q' :
+            #keep asking for input if the numbers of restaurant is not enough
+            if count < 5:
+                print("You have to type at least 5 restaurants: ")
+            #stop when enough restaurants
+            else:
+                break
+        else:
+            query2 = """INSERT INTO restaurant(restaurantname, guestid) VALUES ('%s', (SELECT max(id) FROM guest))""" % (newrestaurant)
+            execute_query(conn, query2)
     return 'POST REQUEST WORKED'
 
 
@@ -132,7 +150,8 @@ def randomrestaurant():
     for object in random_rest:
         results.append(object)
     return jsonify(results)
-    
+
+
 
 
 app.run()
